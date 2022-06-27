2f6f20ca9ba7be05456b60b2926112b23d79b14b
jasquat <jasquat@users.noreply.github.com>
2022-06-27 15:24:13 -0400

    eslint likes single quotes w/ burnettk

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,5 +1,7 @@
 import { slugifyString } from './helpers';
 
 test('it can slugify a string', () => {
-  expect(slugifyString("hello-world_ and then Some such-")).toEqual('hello-world-and-then-some-such');
+  expect(slugifyString('hello-world_ and then Some such-')).toEqual(
+    'hello-world-and-then-some-such'
+  );
 });
