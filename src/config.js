c5e38c2eb3c30c0bf14e42e9d5cbcd88309791be
jasquat <jasquat@users.noreply.github.com>
2022-06-30 09:55:07 -0400

    use stg token on staging

--- src/config.js
+++ src/config.js
@@ -2,8 +2,15 @@ const host = window.location.hostname;
 const hostAndPort = `${host}:7000`;
 
 export const BACKEND_BASE_URL = `http://${hostAndPort}/v1.0`;
-export const HOT_AUTH_TOKEN =
+
+let authTokenForEnv =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOm51bGx9.krsOjlSilPMu_3r7WkkUfKyr-h3HprXr6R4_FXRXz6Y';
+if (hostAndPort.startsWith('167')) {
+  authTokenForEnv =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOm51bGx9.8XDyKOmBisGUqtGWwoEHg_Crvp-2YcxTfAFnCb4L6_k';
+}
+
+export const HOT_AUTH_TOKEN = authTokenForEnv;
 
 export const STANDARD_HEADERS = {
   headers: new Headers({
