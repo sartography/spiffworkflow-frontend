eb96927dc45de15aed1912dd25eae9e658f0c0eb
jasquat <jasquat@users.noreply.github.com>
2022-07-25 14:43:41 -0400

    added new task nav on show page

--- src/routes/TaskShow.tsx
+++ src/routes/TaskShow.tsx
@@ -1,28 +1,38 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Form from '@rjsf/core';
 import { Button, Stack } from 'react-bootstrap';
 
 import HttpService from '../services/HttpService';
+import ErrorContext from '../contexts/ErrorContext';
 
 export default function TaskShow() {
-  const [activeTask, setActiveTask] = useState(null);
   const [task, setTask] = useState(null);
+  const [userTasks, setUserTasks] = useState(null);
   const params = useParams();
   const navigate = useNavigate();
+
+  const setErrorMessage = (useContext as any)(ErrorContext)[1];
 
   useEffect(() => {
     HttpService.makeCallToBackend({
       path: `/tasks/${params.process_instance_id}/${params.task_id}`,
-      successCallback: setActiveTask,
+      successCallback: setTask,
+    });
+    HttpService.makeCallToBackend({
+      path: `/process-instance/${params.process_instance_id}/tasks`,
+      successCallback: setUserTasks,
     });
   }, [params]);
 
   const processSubmitResult = (result: any) => {
+    setErrorMessage('');
     if (result.ok) {
       navigate(`/tasks`);
-    } else {
+    } else if (result.process_instance_id) {
       navigate(`/tasks/${result.process_instance_id}/${result.id}`);
+    } else {
+      setErrorMessage(`Received unexpected error: ${result.message}`);
     }
   };
 
@@ -35,92 +45,69 @@ export default function TaskShow() {
     });
   };
 
-  const gotToOtherTask = (
-    taskId: any,
-    processInstanceId: any,
-    taskType: string = 'previous'
-  ) => {
-    if (taskId) {
-      let text = '<< Previous Form';
-      let path = `/tasks/${taskId}/${processInstanceId}`;
-      if (taskType === 'following') {
-        text = 'Next Form >>';
-      } else if (taskType === 'current') {
-        text = 'Current Active Task';
-        path = `/tasks/${taskId}`;
-      }
-
-      return (
-        <Button variant="secondary" href={path}>
-          {text}
-        </Button>
-      );
+  const buildTaskNavigation = () => {
+    let userTasksElement;
+    if (userTasks) {
+      userTasksElement = (userTasks as any).map(function getUserTasksElement(
+        userTask: any
+      ) {
+        const taskUrl = `/tasks/${params.process_instance_id}/${userTask.id}`;
+        if (userTask.id === params.task_id) {
+          return <span>{userTask.name}</span>;
+        }
+        if (userTask.state === 'COMPLETED') {
+          return <Link to={taskUrl}>{userTask.name}</Link>;
+        }
+        if (userTask.state === 'FUTURE') {
+          return <span style={{ color: 'red' }}>{userTask.name}</span>;
+        }
+        if (userTask.state === 'READY') {
+          return <Link to={taskUrl}>{userTask.name}</Link>;
+        }
+        return null;
+      });
     }
-    return <main />;
+    return (
+      <Stack direction="horizontal" gap={3}>
+        <Button href="/tasks">Go Back To List</Button>
+        {userTasksElement}
+      </Stack>
+    );
   };
 
-  if (activeTask) {
-    const activeTaskToUse = activeTask as any;
+  if (task) {
+    const taskToUse = task as any;
 
     let formUiSchema;
-    if (activeTaskToUse.form_ui_schema) {
-      formUiSchema = JSON.parse(activeTaskToUse.form_ui_schema);
+    if (taskToUse.form_ui_schema) {
+      formUiSchema = JSON.parse(taskToUse.form_ui_schema);
+    }
+
+    let reactFragmentToHideSubmitButton = null;
+    if (taskToUse.state !== 'READY') {
+      formUiSchema = Object.assign(formUiSchema || {}, {
+        'ui:readonly': true,
+      });
+
+      // It doesn't seem as if Form allows for removing the default submit button
+      // so passing a blank fragment or children seem to do it though
+      //
+      // from: https://github.com/rjsf-team/react-jsonschema-form/issues/1602
+      reactFragmentToHideSubmitButton = <div />;
     }
 
     return (
       <main>
-        <Stack direction="horizontal" gap={3}>
-          <Button href="/tasks">Go Back To List</Button>
-          {gotToOtherTask(
-            activeTaskToUse.preceding_spiffworkflow_user_task_id,
-            activeTaskToUse.process_instance_id
-          )}
-        </Stack>
-        <h1>Task ID: {activeTaskToUse.id}</h1>
-        <h3>process_instance_id: {activeTaskToUse.process_instance_id}</h3>
-        <h3>status: {activeTaskToUse.state}</h3>
-        <Form
-          formData={activeTaskToUse.data}
-          onSubmit={handleFormSubmit}
-          schema={JSON.parse(activeTaskToUse.form_schema)}
-          uiSchema={formUiSchema}
-        />
-      </main>
-    );
-  }
-
-  // <Stack direction="horizontal" gap={3}>
-  //   <Button href="/tasks">Go Back To List</Button>
-  //   {gotToOtherTask(
-  //     taskToUse.preceding_spiffworkflow_user_task_id,
-  //     params.process_instance_id
-  //   )}
-  //   {gotToOtherTask(
-  //     taskToUse.following_spiffworkflow_user_task_id,
-  //     params.process_instance_id,
-  //     'following'
-  //   )}
-  //   {gotToOtherTask(
-  //     taskToUse.current_active_task_id,
-  //     params.process_instance_id,
-  //     'current'
-  //   )}
-  // </Stack>
-  if (task) {
-    const taskToUse = task as any;
-    const uiSchema = Object.assign(taskToUse.form_ui_schema || {}, {
-      'ui:readonly': true,
-    });
-    return (
-      <main>
-        <h3>process_instance_id: {params.process_instance_id}</h3>
+        {buildTaskNavigation()}
+        <h1>Task: {taskToUse.name}</h1>
         <h3>status: {taskToUse.state}</h3>
         <Form
           formData={taskToUse.data}
+          onSubmit={handleFormSubmit}
           schema={JSON.parse(taskToUse.form_schema)}
-          uiSchema={uiSchema}
+          uiSchema={formUiSchema}
         >
-          <div />
+          {reactFragmentToHideSubmitButton}
         </Form>
       </main>
     );
