215cb33630f2321cb6765cc990a1ddf6109760a3
jasquat <jasquat@users.noreply.github.com>
2022-09-20 15:23:18 -0400

    removed console log

--- src/routes/MessageInstanceList.tsx
+++ src/routes/MessageInstanceList.tsx
@@ -85,7 +85,6 @@ export default function MessageInstanceList() {
         />
       );
     }
-    console.log('breadcrumbElement', breadcrumbElement);
     return (
       <main>
         {breadcrumbElement}
