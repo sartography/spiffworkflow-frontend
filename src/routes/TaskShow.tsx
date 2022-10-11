c17cc48a370986a2e35e89a8fb9d59279b5dff3b
jasquat <jasquat@users.noreply.github.com>
2022-10-11 10:43:53 -0400

    set the error message when failing to load a task w/ burnettk

--- src/routes/TaskShow.tsx
+++ src/routes/TaskShow.tsx
@@ -15,15 +15,17 @@ export default function TaskShow() {
   const setErrorMessage = (useContext as any)(ErrorContext)[1];
 
   useEffect(() => {
+    setErrorMessage('');
     HttpService.makeCallToBackend({
       path: `/tasks/${params.process_instance_id}/${params.task_id}`,
       successCallback: setTask,
+      failureCallback: setErrorMessage,
     });
     HttpService.makeCallToBackend({
       path: `/process-instance/${params.process_instance_id}/tasks`,
       successCallback: setUserTasks,
     });
-  }, [params]);
+  }, [params, setErrorMessage]);
 
   const processSubmitResult = (result: any) => {
     setErrorMessage('');
